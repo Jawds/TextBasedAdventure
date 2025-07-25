0001257abcc4f0b23fa5057f777a809c69f4c14b
Jawds <jtbrinks@icloud.com>
2025-07-25 23:02:22 +0930

    save function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <chrono>
+#include <thread>
+#include <cstdlib>
 
 int main() {
-    int a;
-    std::cin >> a;
 
-    std::cout << a;
+    std::string PlayerName;
+
+
+    //TODO : Put a loop here that asks the player to confirm what their name is..
+    char userAnswer;
+    std::cout << "What is your name..?\n";
+    std::cin >> PlayerName;
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    std::cout << "Your name is .. " << PlayerName << "? (Y/N)";
+    
+    
+    
+    
+    
+    system("cls");
+
+    std::cout << "Your name has been recorded.\n" << std::endl;
+    
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+
+    system("cls");
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));
     return 0;
 }
+
+// make a separate file for saving user variables like name, location, items in inventory etc.
---
+++ save.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string>
+
+void save_player_name(std::string Name);
